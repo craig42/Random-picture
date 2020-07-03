369964816f997479ba79cfa67f33b35ce1805769
Craig Josse <craig.josse@icloud.com>
2020-07-03 12:16:22 +0200

    Initial Commit

---
+++ Random picture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random picture
+//
+//  Created by Craig Josse on 03/07/2020.
+//  Copyright © 2020 Craig Josse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
